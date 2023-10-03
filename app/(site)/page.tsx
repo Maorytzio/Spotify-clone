01925b0bf86caf8a18f6d34b8c3f4e0dd0d92d9d
Maor Beeri <marmaor@gmail.com>
2023-10-03 11:36:18 +0300

    add sign up and login buttons

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,10 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
-  return(
-    <div className="
+  return (
+    <div
+      className="
       bg-neutral-900
       rounded-lg
       h-full
@@ -12,8 +14,15 @@ export default function Home() {
     "
     >
       <Header>
-        
+        <div className="mb-2 text-3xl font-semibold">
+          <h1>
+            Welcome Back
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem/>
+          </div>
+        </div>
       </Header>
     </div>
-  )
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -112,11 +112,34 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
         >
           <>
             <div>
-              <Button>Sign up</Button>
+              <Button
+                onClick={() => {}}
+                className="
+                bg-transparent
+                text-neutral-300
+                font-medium
+
+              "
+              >
+                Sign up
+              </Button>
+            </div>
+            <div>
+              <Button
+                onClick={() => {}}
+                className="
+                bg-white
+                px-6
+                py-2
+              "
+              >
+                Log in
+              </Button>
             </div>
           </>
         </div>
       </div>
+      {children}
     </div>
   );
 };
