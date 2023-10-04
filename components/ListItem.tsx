8d0afad97fa04d37032cd376f17b153f744f3b64
Maor Beeri <marmaor@gmail.com>
2023-10-04 16:18:52 +0300

    finish Layout

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,35 +1,31 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import {FaPlay} from "react-icons/fa";
 
 interface ListItemProps {
   image: string;
   name: string;
   href: string;
-
 }
 
-const ListItem: React.FC<ListItemProps> = (
-  {
-    image,
-    name,
-    href,
-  }) => {
-    const router = useRouter();
+const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
+  const router = useRouter();
 
-    const onClick = () => {
-      // add authentication before push
-      router.push(href)
-    }
-
+  const onClick = () => {
+    // add authentication before push
+    router.push(href);
+  };
 
   return (
-    <div>
-      <button className="
+    <button
+      onClick={onClick}
+      className="
       relative
       group
       flex
-      item-center
+      items-center
       rounded-md
       overflow-hidden
       gap-x-4
@@ -37,11 +33,45 @@ const ListItem: React.FC<ListItemProps> = (
       hover:bg-neutral-100/20
       transition
       pr-4
-      ">
-        
-      </button>
-    </div>
-  )
-
+      "
+    >
+      <div
+        className="
+          relative
+          min-h-[64px]
+          min-w-[64px]
+        "
+      >
+        <Image 
+         className="object-cover"
+         fill 
+         src={image}
+         alt="image" />
+      </div>
+      <p className="font-medium truncate py-5">
+        {name}
+      </p>
+      <div
+       className="
+       absolute 
+       transition
+       opacity-0
+       rounded-full
+       flex
+       items-cneter
+       justify-center
+       bg-green-500
+       p-4
+       drop-shadow-md
+       right-5
+       group-hover:opacity-100
+       hover:scale-110
+  
+       "
+       >
+        <FaPlay className="text-black"/>
+      </div>
+    </button>
+  );
 };
 export default ListItem;
