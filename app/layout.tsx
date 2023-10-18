157927aa0284d90e3a550ce7882dd83ae77fb9a4
Maor Beeri <marmaor@gmail.com>
2023-10-18 13:01:50 +0300

    started song player functionality, need to fix import bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Sidebar from "@/components/Sidebar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
@@ -8,6 +7,8 @@ import UserProvider from "@/providers/UserProvider";
 import ModalProvider from "@/providers/ModalProvider";
 import ToasterProvider from "@/providers/ToasterProvider";
 import getSongsByUserId from "@/actions/getSongsByUserId";
+import Sidebar from "@/components/Sidebar";
+import Player from "@/components/Player";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -35,6 +36,7 @@ export default async function RootLayout({
             <Sidebar songs={userSongs}>
               {children}
             </Sidebar>
+            <Player/>
           </UserProvider>
         </SupabaseProvider>
       </body>
