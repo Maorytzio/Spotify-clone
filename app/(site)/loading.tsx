a62acebc71714c3f26e4c2325f1fbd768cc151c3
Maor Beeri <marmaor@gmail.com>
2023-10-19 15:10:20 +0300

    add Loading Spinner for Like, Search and Home pages

---
+++ app/(site)/loading.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Box from "@/components/Box";
+import { BeatLoader } from "react-spinners";
+
+const Loading = () => {
+  return (
+    <Box className="h-full flex items-center justify-center">
+      <BeatLoader color="#22c55e" size={20} />
+    </Box>
+  );
+};
+
+export default Loading;
